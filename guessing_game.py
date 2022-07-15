28137ad25b52d871e239b74ef3529370b08f6461
“JuventinaIbarra” <juventinaibarra@gmail.com>
2022-07-15 16:35:29 -0600

    corrections to catch error

--- guessing_game.py
+++ guessing_game.py
@@ -8,8 +8,6 @@ def start_game():
         attemps += 1
         try:
             picked_num = int(input("Pick a number between 1 and 10: "))
-            if picked_num >10 or picked_num <1:
-                raise ValueError("Please, choose a number from 1 to 10")
             if picked_num > num:
                 print("Its lower.")
                 continue
@@ -19,19 +17,9 @@ def start_game():
             elif picked_num == num:
                 print("You got it!! it took you {} tries.".format(attemps))
                 print("The game is over!")
-                high_score = attemps
-                play_again = input("Would you like to play again?    y/n ")
-                if play_again.lower() == "y":
-                    print("The HIGHSCORE is {}".format(high_score))
-                    num = random.randrange(1,10)
-                    attemps = 0
-                    continue
-                else:
-                    print("See you next time")
-                    break
-        except NameError:
-            print("Ooops it was not a valid value. PLEASE TRY AGAIN")
-
+                break
+        except NameError as err:
+            print("Ooops it was not a valid value.  PLEASE TRY AGAIN")
 # Kick off the program by calling the start_game function.
 
 start_game()
